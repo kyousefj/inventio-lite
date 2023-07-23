af9a562bed0236c8ec2417ca68038bcacda25313
Kamal <kyousef@gmail.com>
2023-07-23 01:12:42 -0400

    Mejoras en visualización del home,

--- core/app/view/home-view.php
+++ core/app/view/home-view.php
@@ -1,26 +1,29 @@
-	<?php
-	$found=true;
+<?php
+$found = true;
 $products = ProductData::getAll();
-foreach($products as $product){
-	$q=OperationData::getQYesF($product->id);	
-	if($q<=$product->inventary_min){
-		$found=true;
-		break;
+foreach ($products as $product) {
+  $q = OperationData::getQYesF($product->id);
+  if ($q <= $product->inventary_min) {
+    $found = true;
+    break;
 
-	}
+  }
 }
-	?>
+?>
 <div class="row">
-	<div class="col-md-12">
-		<h1>Bienvenido a Inventio Lite</h1>
-</div>
-</div>
-  <div class="row">
+  <div class="container">
+    <div class="panel panel-primary">
+      <div class="text-center panel-heading">
+        <h1>Bienvenido a Inventio Lite</h1>
+      </div>
+      <div class="panel-body">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
           <div class="small-box bg-aqua">
             <div class="inner">
-              <h3><?php echo count(ProductData::getAll());?></h3>
+              <h3>
+                <?php echo count(ProductData::getAll()); ?>
+              </h3>
 
               <p>Productos</p>
             </div>
@@ -35,7 +38,9 @@ foreach($products as $product){
           <!-- small box -->
           <div class="small-box bg-purple">
             <div class="inner">
-              <h3><?php echo count(PersonData::getClients());?></h3>
+              <h3>
+                <?php echo count(PersonData::getClients()); ?>
+              </h3>
 
               <p>Clientes</p>
             </div>
@@ -50,7 +55,9 @@ foreach($products as $product){
           <!-- small box -->
           <div class="small-box bg-yellow">
             <div class="inner">
-              <h3><?php echo count(PersonData::getProviders());?></h3>
+              <h3>
+                <?php echo count(PersonData::getProviders()); ?>
+              </h3>
 
               <p>Proveedores</p>
             </div>
@@ -65,7 +72,9 @@ foreach($products as $product){
           <!-- small box -->
           <div class="small-box bg-red">
             <div class="inner">
-              <h3><?php echo count(CategoryData::getAll());?></h3>
+              <h3>
+                <?php echo count(CategoryData::getAll()); ?>
+              </h3>
 
               <p>Categorias</p>
             </div>
@@ -75,65 +84,84 @@ foreach($products as $product){
             <a href="./?view=categories" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <!-- ./col -->
+        <div class="col-md-12">
+    <?php if ($found): ?>
+      <div class="btn-group pull-right">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+          <i class="fa fa-download"></i> Descargar <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="report/alerts-word.php">Word 2007 (.docx)</a></li>
+        </ul>
       </div>
-      <!-- /.row -->
+    <?php endif; ?>
 
-<div class="row">
-	<div class="col-md-12">
-<?php if($found):?>
-<div class="btn-group pull-right">
-  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-    <i class="fa fa-download"></i> Descargar <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu">
-    <li><a href="report/alerts-word.php">Word 2007 (.docx)</a></li>
-  </ul>
+  </div>
+  <div class="clearfix"></div>
+  <?php if (count($products) > 0) { ?>
+    <br>
+    <table class="table table-bordered table-hover">
+      <thead>
+        <th>Codigo</th>
+        <th>Nombre del producto</th>
+        <th>En Stock</th>
+        <th></th>
+      </thead>
+      <?php
+      foreach ($products as $product):
+        $q = OperationData::getQYesF($product->id);
+        ?>
+        <?php if ($q <= $product->inventary_min): ?>
+          <tr class="<?php if ($q == 0) {
+            echo "danger";
+          } else if ($q <= $product->inventary_min / 2) {
+            echo "danger";
+          } else if ($q <= $product->inventary_min) {
+            echo "warning";
+          } ?>">
+            <td>
+              <?php echo $product->id; ?>
+            </td>
+            <td>
+              <?php echo $product->name; ?>
+            </td>
+            <td>
+              <?php echo $q; ?>
+            </td>
+            <td>
+              <?php if ($q == 0) {
+                echo "<span class='label label-danger'>No hay existencias.</span>";
+              } else if ($q <= $product->inventary_min / 2) {
+                echo "<span class='label label-danger'>Quedan muy pocas existencias.</span>";
+              } else if ($q <= $product->inventary_min) {
+                echo "<span class='label label-warning'>Quedan pocas existencias.</span>";
+              } ?>
+            </td>
+          </tr>
+        <?php endif; ?>
+        <?php
+      endforeach;
+      ?>
+    </table>
+
+    <div class="clearfix"></div>
+
+    <?php
+  } else {
+    ?>
+    <div class="jumbotron">
+      <h2>No hay alertas</h2>
+      <p>Por el momento no hay alertas de inventario, estas se muestran cuando el inventario ha alcanzado el nivel minimo.</p>
+    </div>
+    <?php
+  }
+
+  ?>
+
+      </div>
+    </div>
+  </div>
+
 </div>
-<?php endif;?>
 
-</div>
-<div class="clearfix"></div>
-<?php if(count($products)>0){?>
-<br><table class="table table-bordered table-hover">
-	<thead>
-		<th >Codigo</th>
-		<th>Nombre del producto</th>
-		<th>En Stock</th>
-		<th></th>
-	</thead>
-	<?php
-foreach($products as $product):
-	$q=OperationData::getQYesF($product->id);
-	?>
-	<?php if($q<=$product->inventary_min):?>
-	<tr class="<?php if($q==0){ echo "danger"; }else if($q<=$product->inventary_min/2){ echo "danger"; } else if($q<=$product->inventary_min){ echo "warning"; } ?>">
-		<td><?php echo $product->id; ?></td>
-		<td><?php echo $product->name; ?></td>
-		<td><?php echo $q; ?></td>
-		<td>
-		<?php if($q==0){ echo "<span class='label label-danger'>No hay existencias.</span>";}else if($q<=$product->inventary_min/2){ echo "<span class='label label-danger'>Quedan muy pocas existencias.</span>";} else if($q<=$product->inventary_min){ echo "<span class='label label-warning'>Quedan pocas existencias.</span>";} ?>
-		</td>
-	</tr>
-<?php endif;?>
-<?php
-endforeach;
-?>
-</table>
-
-<div class="clearfix"></div>
-
-	<?php
-}else{
-	?>
-	<div class="jumbotron">
-		<h2>No hay alertas</h2>
-		<p>Por el momento no hay alertas de inventario, estas se muestran cuando el inventario ha alcanzado el nivel minimo.</p>
-	</div>
-	<?php
-}
-
-?>
-<br><br><br><br><br><br><br><br><br><br>
-	</div>
 </div>
